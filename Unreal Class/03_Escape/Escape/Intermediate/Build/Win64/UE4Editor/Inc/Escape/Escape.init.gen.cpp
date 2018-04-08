8178ba528475e2c8c53890afd6ade3ae4e8c2205
DESKTOP-DBDP50N\ashee <asheen127@comcast.net>
2018-04-08 13:24:45 -0700

    Grabber Start

--- Unreal Class/03_Escape/Escape/Intermediate/Build/Win64/UE4Editor/Inc/Escape/Escape.init.gen.cpp
+++ Unreal Class/03_Escape/Escape/Intermediate/Build/Win64/UE4Editor/Inc/Escape/Escape.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeEscape_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/Escape",
 				PKG_CompiledIn | 0x00000000,
-				0x230994A2,
-				0xC0DAE9E8,
+				0xDBF49504,
+				0xB4076E54,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
--- Unreal Class/03_Escape/Escape/Intermediate/Build/Win64/UE4Editor/Inc/Escape/OpenDoor.gen.cpp
+++ Unreal Class/03_Escape/Escape/Intermediate/Build/Win64/UE4Editor/Inc/Escape/OpenDoor.gen.cpp
@@ -64,7 +64,7 @@ void EmptyLinkFunctionForGeneratedCodeOpenDoor() {}
 				{ "ToolTip", "UPROPERTY is a macro in Unreal. This can be assigned to an actor for any purpose." },
 			};
 #endif
-			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_OpenAngle = { UE4CodeGen_Private::EPropertyClass::Float, "OpenAngle", RF_Public|RF_Transient|RF_MarkAsNative, 0x0040000000020001, 1, nullptr, STRUCT_OFFSET(UOpenDoor, OpenAngle), METADATA_PARAMS(NewProp_OpenAngle_MetaData, ARRAY_COUNT(NewProp_OpenAngle_MetaData)) };
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_OpenAngle = { UE4CodeGen_Private::EPropertyClass::Float, "OpenAngle", RF_Public|RF_Transient|RF_MarkAsNative, 0x0040000000000001, 1, nullptr, STRUCT_OFFSET(UOpenDoor, OpenAngle), METADATA_PARAMS(NewProp_OpenAngle_MetaData, ARRAY_COUNT(NewProp_OpenAngle_MetaData)) };
 			static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[] = {
 				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_DoorCloseDelay,
 				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_PressurePlate,
@@ -88,7 +88,7 @@ void EmptyLinkFunctionForGeneratedCodeOpenDoor() {}
 		}
 		return OuterClass;
 	}
-	IMPLEMENT_CLASS(UOpenDoor, 1870626588);
+	IMPLEMENT_CLASS(UOpenDoor, 1880030643);
 	static FCompiledInDefer Z_CompiledInDefer_UClass_UOpenDoor(Z_Construct_UClass_UOpenDoor, &UOpenDoor::StaticClass, TEXT("/Script/Escape"), TEXT("UOpenDoor"), false, nullptr, nullptr, nullptr);
 	DEFINE_VTABLE_PTR_HELPER_CTOR(UOpenDoor);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
